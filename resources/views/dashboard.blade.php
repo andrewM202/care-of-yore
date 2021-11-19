0282f6eec49629aeede7799e175ff7f35c68d379
Spencer Heffley <spheffley@yahoo.com>
2021-11-19 07:56:25 -0500

    Added users name to home page. Removed optional values for navigation

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,8 @@
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <h3 class='font-semibold text-lg text-gray-800 leading-tight'>Home Page</h3>
-                    <section class="container mx-auto p-6 font-mono">
+                    <h3 class='font-semibold text-lg text-gray-800 leading-tight'>{{ Auth::user()->name }}'s Page</h3>
+                    <section class="container mx-auto font-mono">
                         <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
                           <div class="w-full overflow-x-auto">
                             <table class="w-full">
@@ -34,7 +34,9 @@
                                         Dr. Arafat
                                         </div>
                                     </td>
-                                    <td class="px-4 py-3 text-ms font-semibold border">12:00</td>
+                                    <td class="px-4 py-3 text-ms font-semibold border">
+                                        <input type='checkbox'>
+                                    </td>
                                     <td class="px-4 py-3 text-sm border">
                                         Mainul Chowdhury
                                     </td>
