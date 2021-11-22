b0ee47f4d17f75928ae4e7bc5007471c7da47b23
Spencer Heffley <spheffley@yahoo.com>
2021-11-22 08:46:05 -0500

    Completed styling for caregiver home

--- resources/views/components/caregiver-home.blade.php
+++ resources/views/components/caregiver-home.blade.php
@@ -1,90 +1,55 @@
 <h3 class='font-semibold text-lg text-gray-800 leading-tight'>Caregiver's Home</h3>
 
-<section class="container mx-auto mt-5">
-    <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
-        <div class="w-full overflow-x-auto">
-            <table class="w-full">
-                <thead>
-                    <tr class="text-md font-semibold text-left text-gray-900 bg-gray-100 border-b border-gray-600">
-                        <th class="px-4 py-3">Name</th>
-                        <th class="px-4 py-3">Morning Med</th>
-                        <th class="px-4 py-3">Afternoon Med</th>
-                        <th class="px-4 py-3">Night Med</th>
-                        <th class="px-4 py-3">Breakfast</th>
-                        <th class="px-4 py-3">Lunch</th>
-                        <th class="px-4 py-3">Dinner</th>
-                    </tr>
-                </thead>
-                <tbody class="bg-white">
-                    <tr class="text-gray-700">
-                        <td class="px-4 py-3 border">
-                            <div class="flex items-center text-sm">
-                                Dr. Arafat
-                            </div>
-                        </td>
-                        <td class="px-4 py-3 text-ms font-semibold border">
-                            <input type='checkbox' disabled>
-                        </td>
-                        <td class="px-4 py-3 text-ms font-semibold border">
-                            <input type='checkbox' disabled>
-                        </td>
-                        <td class="px-4 py-3 text-sm border">
-                            <input type='checkbox' disabled>
-                        </td>
-                        <td class="px-4 py-3 text-sm border">
-                            <input type='checkbox' disabled>
-                        </td>
-                        <td class="px-4 py-3 text-sm border">
-                            <input type='checkbox' disabled>
-                        </td>
-                        <td class="px-4 py-3 text-sm border">
-                            <input type='checkbox' disabled>
-                        </td>
-                    </tr>
+<form>
+    <section class="container mx-auto mt-5">
+        <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+            <div class="w-full overflow-x-auto">
+                <table class="w-full">
+                    <thead>
+                        <tr class="text-md font-semibold text-left text-gray-900 bg-gray-100 border-b border-gray-600">
+                            <th class="px-4 py-3">Name</th>
+                            <th class="px-4 py-3">Morning Med</th>
+                            <th class="px-4 py-3">Afternoon Med</th>
+                            <th class="px-4 py-3">Night Med</th>
+                            <th class="px-4 py-3">Breakfast</th>
+                            <th class="px-4 py-3">Lunch</th>
+                            <th class="px-4 py-3">Dinner</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white">
+                        <tr class="text-gray-700">
+                            <td class="px-4 py-3 border">
+                                <div class="flex items-center text-sm">
+                                    Dr. Arafat
+                                </div>
+                            </td>
+                            <td class="px-4 py-3 text-ms font-semibold border">
+                                <input type='checkbox'>
+                            </td>
+                            <td class="px-4 py-3 text-ms font-semibold border">
+                                <input type='checkbox'>
+                            </td>
+                            <td class="px-4 py-3 text-sm border">
+                                <input type='checkbox'>
+                            </td>
+                            <td class="px-4 py-3 text-sm border">
+                                <input type='checkbox'>
+                            </td>
+                            <td class="px-4 py-3 text-sm border">
+                                <input type='checkbox'>
+                            </td>
+                            <td class="px-4 py-3 text-sm border">
+                                <input type='checkbox'>
+                            </td>
+                        </tr>
 
-                </tbody>
-            </table>
-        </div>
-    </div>
-</section>
-
-<section>
-    <form>
-        <div class="grid grid-cols-3 gap-4 m-5">
-            <p>Appointments</p>
-            <div>
-                <label for='date'>Till Date</label>
-                <input type='date' placeholder='Till Date' name='date' value='date'>
+                    </tbody>
+                </table>
             </div>
-            <x-button>Submit</x-button>
         </div>
-    </form>
-</section>
-
-<section class="container mx-auto mt-5">
-    <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
-        <div class="w-full overflow-x-auto">
-            <table class="w-full">
-                <thead>
-                    <tr class="text-md font-semibold text-left text-gray-900 bg-gray-100 border-b border-gray-600">
-                        <th class="px-4 py-3">Patient</th>
-                        <th class="px-4 py-3">Date</th>
-                    </tr>
-                </thead>
-                <tbody class="bg-white">
-                    <tr class="text-gray-700">
-                        <td class="px-4 py-3 border">
-                            <div class="flex items-center text-sm">
-                                Dr. Arafat
-                            </div>
-                        </td>
-                        <td class="px-4 py-3 text-ms font-semibold border">
-                            <input type='checkbox' disabled>
-                        </td>
-                    </tr>
-
-                </tbody>
-            </table>
+        <div class='float-right m-5'>
+            <x-button>Ok</x-button>
+            <x-button type='reset'>Cancel</x-button>
         </div>
-    </div>
-</section>
+    </section>
+</form>
