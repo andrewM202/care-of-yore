ea4a6fac073c89b63ac2ccc3d11dfd5157fbc6f1
Spencer Heffley <spheffley@yahoo.com>
2021-11-22 10:25:48 -0500

    Updated styling for roles page

--- resources/views/roles.blade.php
+++ resources/views/roles.blade.php
@@ -1,31 +1,65 @@
 <x-app-layout>
     <body>
-        <div id='roles'>
+        <div id='roles' class='m-10'>
             <table class='table-auto'>
                 <thead>
                     <tr class='text-md font-semibold text-left text-gray-900 bg-gray-100 border-b border-gray-600'>
-                        <th>Role</th>
-                        <th>Access Level</th>
+                        <th class="px-4 py-3">Role</th>
+                        <th class="px-4 py-3">Access Level</th>
                     </tr>
                 </thead>
                 <tbody class='bg-white'>
-                    <tr class='text-gray-700'>
-                        <td>Admin</td>
-                        <td>0</td>
+                    <tr class='text-gray-700 font-semibold'>
+                        <td class="px-4 py-3 border">Admin</td>
+                        <td class="px-4 py-3 border">0</td>
+                    </tr>
+                    <tr class='text-gray-700 font-semibold'>
+                        <td class="px-4 py-3 border">Admin</td>
+                        <td class="px-4 py-3 border">0</td>
+                    </tr>
+                    <tr class='text-gray-700 font-semibold'>
+                        <td class="px-4 py-3 border">Admin</td>
+                        <td class="px-4 py-3 border">0</td>
+                    </tr>
+                    <tr class='text-gray-700 font-semibold'>
+                        <td class="px-4 py-3 border">Admin</td>
+                        <td class="px-4 py-3 border">0</td>
+                    </tr>
+                    <tr class='text-gray-700 font-semibold'>
+                        <td class="px-4 py-3 border">Admin</td>
+                        <td class="px-4 py-3 border">0</td>
+                    </tr>
+                    <tr class='text-gray-700 font-semibold'>
+                        <td class="px-4 py-3 border">Admin</td>
+                        <td class="px-4 py-3 border">0</td>
+                    </tr>
+                    <tr class='text-gray-700 font-semibold'>
+                        <td class="px-4 py-3 border">Admin</td>
+                        <td class="px-4 py-3 border">0</td>
+                    </tr>
+                    <tr class='text-gray-700 font-semibold'>
+                        <td class="px-4 py-3 border">Admin</td>
+                        <td class="px-4 py-3 border">0</td>
+                    </tr>
+                    <tr class='text-gray-700 font-semibold'>
+                        <td class="px-4 py-3 border">Admin</td>
+                        <td class="px-4 py-3 border">0</td>
                     </tr>
                 </tbody>
             </table>
         </div>
 
-        <form method="POST">
+        <form method="POST" class='m-10'>
             <div id='roleCreation' class='flex flex-col max-w-sm'>
                 <label for='newRole'>New Role</label>
                 <input type='text' placeholder='Patient' name='newRole' class=''>
-                <label for='accessLevel'>Access Level</label>
+                <label for='accessLevel' class='mt-5'>Access Level</label>
                 <input type='number' placeholder='5' min="0" name='accessLevel'>
             </div>
-            <button type="submit" class='p-3 border border-gray-700'>Ok</button>
-            <button type="reset" class='p-3 border border-gray-700'>Cancel</button>
+            <div class='mt-5'>
+                <x-button type="submit">Ok</x-button>
+                <x-button type="reset">Cancel</x-button>
+            </div>
         </form>
     </body>
 </x-app-layout>
