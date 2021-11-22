ce4584be4743f76df6a2aaf04729725142b48fa1
Gabe <gabecurran01@gmail.com>
2021-11-22 09:26:21 -0500

    Add basic styling for buttons and inputs

--- resources/views/roles.blade.php
+++ resources/views/roles.blade.php
@@ -3,15 +3,15 @@
         <div id='roles'>
             <table class='table-auto'>
                 <thead>
-                    <tr>
+                    <tr class='text-md font-semibold text-left text-gray-900 bg-gray-100 border-b border-gray-600'>
                         <th>Role</th>
                         <th>Access Level</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                        <td>Admin</th>
-                        <td>0</th>
+                <tbody class='bg-white'>
+                    <tr class='text-gray-700'>
+                        <td>Admin</td>
+                        <td>0</td>
                     </tr>
                 </tbody>
             </table>
@@ -20,12 +20,12 @@
         <form method="POST">
             <div id='roleCreation'>
                 <label for='newRole'>New Role</label>
-                <input type='text' name='newRole'>
+                <input type='text' placeholder='Patient' name='newRole' class=''>
                 <label for='accessLevel'>Access Level</label>
-                <input type='number' min="0" name='accessLevel'>
+                <input type='number' placeholder='5' min="0" name='accessLevel'>
             </div>
-            <button type="submit">Ok</button>
-            <button type="reset">Cancel</button>
+            <button type="submit" class='p-3 border border-gray-700'>Ok</button>
+            <button type="reset" class='p-3 border border-gray-700'>Cancel</button>
         </form>
     </body>
 </x-app-layout>
