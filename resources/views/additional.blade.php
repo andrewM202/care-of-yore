5b939273017efe7edf0d3a2afa79fafee8787af7
Gabe <gabecurran01@gmail.com>
2021-11-24 10:43:52 -0500

    Add Spencer's Header

--- resources/views/additional.blade.php
+++ resources/views/additional.blade.php
@@ -5,6 +5,11 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                 <div class='flex flex-row justify-center'>
+                <x-slot name="header">
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                        {{ __('Additional Information of Patient') }}
+                    </h2>
+                </x-slot>
                         <form method="POST" class='m-10'>
                             <div id='addPatientInfo' class='flex flex-row'>
                                 <div id='fillablePatientInfo' class='flex flex-col'>
