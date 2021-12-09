f0936471bed7a77b1a8223a2717a84fa79e0b69b
Andrew <andrewm1246@gmail.com>
2021-12-08 19:37:04 -0500

    Refactored employee list view

--- resources/views/employee-list.blade.php
+++ resources/views/employee-list.blade.php
@@ -1,141 +1,86 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Employee List') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="flex flex-col sm:flex-row justify-center p-6 bg-white border-b border-gray-200">
-                    <div class="mx-8 bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
-                     <h3 class="text-xl leading-none font-bold text-gray-900 mb-10">Employees</h3>
-                     <div class="block w-full overflow-x-auto">
-                        <table class="items-center w-full bg-transparent border-collapse">
-                           <thead>
-                              <tr>
-                                 <th class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">ID</th>
-                                 <th class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">Name</th>
-                                 <th class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">Role</th>
-                                 <th class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">Salary</th>
-                              </tr>
-                           </thead>
-                           <tbody class="divide-y divide-gray-100">
-                              <tr class="text-gray-500">
-                                 <th class="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Organic Search</th>
-                                 <td class="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">5,649</td>
-                                 <td class="border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
-                                    <div class="flex items-center">
-                                       <span class="mr-2 text-xs font-medium">30%</span>
-                                       <div class="relative w-full">
-                                          <div class="w-full bg-gray-200 rounded-sm h-2">
-                                             <div class="bg-cyan-600 h-2 rounded-sm" style="width: 30%"></div>
-                                          </div>
+    <body>
+        <div class="py-12">
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <!-- <div class='flex flex-row justify-center'> -->
+                            <x-slot name="header">
+                                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                                    {{ __('View Roster') }}
+                                </h2>
+                            </x-slot>
+                            <div class="flex-col">
+                                <form method="GET" class='m-10' action={{ route('get-roster') }}>
+                                    @csrf 
+                                    <div class="flex flex-col md:flex-row">
+                                       <div id='roleCreation' class='flex flex-col max-w-sm mx-2'>
+                                          <label for='newRole'>ID</label>
+                                          <x-input type='text' name='roster_date'/>
+                                       </div>
+                                       <div id='roleCreation' class='flex flex-col max-w-sm mx-2'>
+                                          <label for='newRole'>Name</label>
+                                          <x-input type='text' name='roster_date'/>
+                                       </div>
+                                       <div id='roleCreation' class='flex flex-col max-w-sm mx-2'>
+                                          <label for='newRole'>Roll</label>
+                                          <x-input type='text' name='roster_date'/>
+                                       </div>
+                                       <div id='roleCreation' class='flex flex-col max-w-sm mx-2'>
+                                          <label for='newRole'>Salary</label>
+                                          <x-input type='text' name='roster_date'/>
                                        </div>
                                     </div>
-                                 </td>
-                              </tr>
-                              <tr class="text-gray-500">
-                                 <th class="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Referral</th>
-                                 <td class="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">4,025</td>
-                                 <td class="border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
-                                    <div class="flex items-center">
-                                       <span class="mr-2 text-xs font-medium">24%</span>
-                                       <div class="relative w-full">
-                                          <div class="w-full bg-gray-200 rounded-sm h-2">
-                                             <div class="bg-orange-300 h-2 rounded-sm" style="width: 24%"></div>
-                                          </div>
+                                    <div class='mt-5'>
+                                        <x-button type="submit">Search</x-button>
+                                    </div>
+                                </form>
+                                <form method="GET" class='m-10' action={{ route('get-roster') }}>
+                                    @csrf 
+                                    <div class="flex flex-col md:flex-row">
+                                       <div id='roleCreation' class='flex flex-col max-w-sm mx-2'>
+                                          <label for='newRole'>ID</label>
+                                          <x-input type='text' name='roster_date'/>
+                                       </div>
+                                       <div id='roleCreation' class='flex flex-col max-w-sm mx-2'>
+                                          <label for='newRole'>New Salary</label>
+                                          <x-input type='text' name='roster_date'/>
                                        </div>
                                     </div>
-                                 </td>
-                              </tr>
-                              <tr class="text-gray-500">
-                                 <th class="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Direct</th>
-                                 <td class="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">3,105</td>
-                                 <td class="border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
-                                    <div class="flex items-center">
-                                       <span class="mr-2 text-xs font-medium">18%</span>
-                                       <div class="relative w-full">
-                                          <div class="w-full bg-gray-200 rounded-sm h-2">
-                                             <div class="bg-teal-400 h-2 rounded-sm" style="width: 18%"></div>
-                                          </div>
-                                       </div>
+                                    <div class='mt-5'>
+                                        <x-button type="submit">Submit</x-button>
                                     </div>
-                                 </td>
-                              </tr>
-                              <tr class="text-gray-500">
-                                 <th class="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Social</th>
-                                 <td class="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">1251</td>
-                                 <td class="border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
-                                    <div class="flex items-center">
-                                       <span class="mr-2 text-xs font-medium">12%</span>
-                                       <div class="relative w-full">
-                                          <div class="w-full bg-gray-200 rounded-sm h-2">
-                                             <div class="bg-pink-600 h-2 rounded-sm" style="width: 12%"></div>
-                                          </div>
-                                       </div>
-                                    </div>
-                                 </td>
-                              </tr>
-                              <tr class="text-gray-500">
-                                 <th class="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Other</th>
-                                 <td class="border-t-0 px-4 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4">734</td>
-                                 <td class="border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
-                                    <div class="flex items-center">
-                                       <span class="mr-2 text-xs font-medium">9%</span>
-                                       <div class="relative w-full">
-                                          <div class="w-full bg-gray-200 rounded-sm h-2">
-                                             <div class="bg-indigo-600 h-2 rounded-sm" style="width: 9%"></div>
-                                          </div>
-                                       </div>
-                                    </div>
-                                 </td>
-                              </tr>
-                              <tr class="text-gray-500">
-                                 <th class="border-t-0 align-middle text-sm font-normal whitespace-nowrap p-4 pb-0 text-left">Email</th>
-                                 <td class="border-t-0 align-middle text-xs font-medium text-gray-900 whitespace-nowrap p-4 pb-0">456</td>
-                                 <td class="border-t-0 align-middle text-xs whitespace-nowrap p-4 pb-0">
-                                    <div class="flex items-center">
-                                       <span class="mr-2 text-xs font-medium">7%</span>
-                                       <div class="relative w-full">
-                                          <div class="w-full bg-gray-200 rounded-sm h-2">
-                                             <div class="bg-purple-500 h-2 rounded-sm" style="width: 7%"></div>
-                                          </div>
-                                       </div>
-                                    </div>
-                                 </td>
-                              </tr>
-                           </tbody>
-                        </table>
-                     </div>
-                    </div>
-                    <div class="flex items-center h-full">
-                        <div class='h-full grid grid-rows-2 gap-4'>
-                            <div class="flex justify-center flex-col">
-                                <x-label for='employee-id' :value="__('Employee ID')" />
-                                <x-input type="text" id="employee-id" class="form-input" />
+                                </form>
                             </div>
-                            <div class="flex justify-center flex-col">
-                                <x-label for='employee-name' :value="__('Employee Name')" />
-                                <x-input type="text" id="employee-name" class="form-input" />
-                            </div>
-                            <div class="flex justify-center flex-col">
-                                <x-label for='employee-role' :value="__('Employee Role')" />
-                                <x-input type="text" id="employee-role" class="form-input" />
-                            </div>
-                            <div class="flex justify-center flex-col">
-                                <x-label for='new-salary' :value="__('New Salary')" />
-                                <x-input type="text" id="new-salary" class="form-input" />
-                            </div>
-                            <div class="flex justify-center">
-                                <x-button class="mx-4">Ok</x-button>
-                                <x-button type='reset' class="mx-4">Cancel</x-button>
-                            </div>
-                        </div>
+                                <x-input type='hidden' name="roster_date" value='<?php echo date("m/d/Y"); ?>' />
+                                <h1 class="mb-1.5 text-center text-2xl">
+                                    <?php echo date("m/d/Y") ?>
+                                </h1>
+                                <x-input type='hidden' name="roster_date" value="" />
+                                <h1 class="mb-1.5 text-center text-2xl">test
+                                </h1>
+                                <table class='table-auto w-full'>
+                                    <thead>
+                                        <tr class='text-md font-semibold text-left text-gray-900 bg-gray-100 border-b border-gray-600'>
+                                             <th class="px-4 py-3">ID</th>
+                                             <th class="px-4 py-3">Name</th>
+                                             <th class="px-4 py-3">Role</th>
+                                             <th class="px-4 py-3">Salary</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class='bg-white'>
+                                        <tr class='text-gray-700 font-semibold'>
+                                            <td class="px-4 py-3 border">Not Set</td>
+                                            <td class="px-4 py-3 border">Not Set</td>
+                                            <td class="px-4 py-3 border">Not Set</td>
+                                            <td class="px-4 py-3 border">Not Set</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                        <!-- </div> -->
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </body>
 </x-app-layout>
