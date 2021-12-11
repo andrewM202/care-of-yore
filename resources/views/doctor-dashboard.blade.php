e45ed4b0443c5f70a9b82797be586146c4ade48e
Andrew <andrewm1246@gmail.com>
2021-12-10 21:44:19 -0500

    Adjusted doctor dashboard layout

--- resources/views/doctor-dashboard.blade.php
+++ resources/views/doctor-dashboard.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white">
                     <h3 class='font-semibold text-lg text-gray-800 leading-tight'>Doctor's Home</h3>
@@ -61,27 +61,21 @@
                                          @endforeach
                                     </tbody>
                                 </table>
-                                <x-button type='submit'>
-                                    Update
-                                </x-button>
                             </div>
                         </div>
                     </section>
 
-                    <section>
+                    <section class="flex">
                         <form action='{{ route('doctor-dashboard') }}' method='get'>
-                            <div class="grid grid-cols-3 gap-4 m-5">
+                            <div class="flex flex-col mx-4">
                                 <p>Appointments</p>
-                                <div>
+                                <div class="py-4">
                                     <x-label for='till-date' :value="__('Till Date')" />
                                     <x-input type="date" name='till-date' id="till-date" class="form-input" />
                                 </div>
-                                <x-button>Submit</x-button>
+                                <x-button class="w-32 flex justify-center">Submit</x-button>
                             </div>
                         </form>
-                    </section>
-
-                    <section class="container mx-auto mt-5">
                         <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
                             <div class="w-full overflow-x-auto">
                                 <table class="w-full">
@@ -109,6 +103,35 @@
                             </div>
                         </div>
                     </section>
+
+                    <!-- <section class="container mx-auto mt-5">
+                        <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+                            <div class="w-full overflow-x-auto">
+                                <table class="w-full">
+                                    <thead>
+                                        <tr class="text-md font-semibold text-left text-gray-900 bg-gray-100 border-b border-gray-600">
+                                            <th class="px-4 py-3">Patient</th>
+                                            <th class="px-4 py-3">Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="bg-white">
+                                        @foreach ($appointmentsTill as $appointment)
+                                            <tr class="text-gray-700">
+                                                <td class="px-4 py-3 border">
+                                                    <div class="flex items-center text-sm">
+                                                        {{ $appointment->patient_name }}
+                                                    </div>
+                                                </td>
+                                                <td class="px-4 py-3 text-sm border">
+                                                    {{ $appointment->appointment_date }}
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </section> -->
                 </div>
             </div>
         </div>
